e533a401bebfa6ae758456b601ae83deced1b497
Ch12hdez <scorpio110699@gmail.com>
2019-07-26 15:04:44 -0500

    Juego Pre-Entrega
    Base del juego

---
+++ Volcanic Apocalypse/Assets/Scripts/Vida.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Vida : MonoBehaviour
+{
+    public float vida;
+    public GameObject[] vidas;
+    public Slider barraVida;
+    // Start is called before the first frame update
+    void Start()
+    {
+        barraVida.value = vida;
+        //vidas = new GameObject[(int)vida];
+        //vidas = GameObject.FindGameObjectsWithTag("MarcadorVida");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            vida--;
+            vidas[(int)vida].SetActive(false);
+            barraVida.value = vida;
+        }
+    }
+}
